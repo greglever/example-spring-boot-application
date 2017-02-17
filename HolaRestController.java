c3550f281f65532540cae2e07977744a99a96f65
Greg Lever <greglever@gmail.com>
2017-02-17 15:30:26 +0000

    Update HolaRestController.java

--- HolaRestController.java
+++ HolaRestController.java
@@ -1,13 +1,14 @@
 @RestController 
 @RequestMapping("/api")
 public class HolaRestController {
-    @RequestMapping(method = RequestMethod.GET, value = "/hola", produces = "text/plain")
-public String hola() throws UnknownHostException { String hostname = null;
-	try {
-            hostname = InetAddress.getLocalHost().getHostAddress();
-	} catch (UnknownHostException e) { 
-		hostname = "unknown";
-		}	
-		return "Hola Spring Boot de " + hostname; 
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/hola", produces = "text/plain")
+	public String hola() throws UnknownHostException { String hostname = null;
+		try {
+		    hostname = InetAddress.getLocalHost().getHostAddress();
+		} catch (UnknownHostException e) { 
+			hostname = "unknown";
+			}	
+			return "Hola Spring Boot de " + hostname; 
+		}
 	}
-}
