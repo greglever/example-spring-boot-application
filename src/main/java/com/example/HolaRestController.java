d7687e0e39f783dacb003f0549ddcf37e86bcee0
Greg Lever <greglever@gmail.com>
2017-02-18 17:43:46 +0000

    updated api/hola endpoint

--- src/main/java/com/example/HolaRestController.java
+++ src/main/java/com/example/HolaRestController.java
@@ -1,3 +1,5 @@
+package com.example;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.net.UnknownHostException;
